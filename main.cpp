0c8b6f6f683d60a2ce7632705f6f9dd8d28060af
Seth Rivers <davmrive@ut.utm.edu>
2024-01-24 15:38:03 -0600

    removed Hello World, added sum program.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 
 #include <iostream>
-
 using namespace std;
 
+/*gets a sum by adding the numbers 1 - n
+ *n: a numerical integar
+ */
+int sum(int n);
+
 int main() {
+  int n;
   
-  cout<<"Hello World!"<<endl;
+  cout<<"Please type in a number: ";
+  cin>>n;
+  int s = sum(n);
+  cout<<"The sum of the numbers 1 - "<<n<<" is: "<<s<<endl;
   
   return 0;
+}
+
+int sum(int n){
+  int sum = n;
+
+  for (int i = 1; i < n; i++){
+    sum += i;
+  }
+
+  return sum;
 }
