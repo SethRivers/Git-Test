1de41ecb808055fa2fa6f394fd76b2ebd9c557eb
Seth Rivers <davmrive@ut.utm.edu>
2024-02-01 16:41:23 -0600

    attempted number 2 of the HW0.5 quiz redo questions; pointers are definetly my weakspot.

--- HW_0.5/main.cpp
+++ HW_0.5/main.cpp
@@ -14,92 +14,96 @@
 #include <string>
 using namespace std;
 
-const int MAX_ARRAY = 1000;
-
-
 /**
  * A function that populates the array with 100 integars from the txt file "integars"
  *
- * @param int array[] an array designed to hold a predetermined number of integars.
+ * @param int array[] A dynamic array designed to hold a predetermined number of integars.
  * @pre 
  * @return int 
  * @post 
  * 
  */
-int populate(int array[]);
+void populate(int array[], int length);
 
 
 /**
  * a function that averages the integars inside the array.
  *
- * @param int array[] an array designed to hold a predetermined number of integars. 
- * @param int total the total amount of integars inside the array
+ * @param int array[] a Dynamic Array designed to hold a predetermined number of integars. 
+ * @param int length the total amount of integars inside the array
  * @pre 
- * @return float 
  * @post 
  * 
  */
-float average(int array[], int total);
+float average(int array[], int length);
 
 
 /**
  * a function that prints the contents of the array
  *
  * @param int array[] an array designed to hold a predetermined number of integars
- * @param int total the total amount of integars inside the array
+ * @param int length the total amount of integars inside the array
  * @pre 
  * @return void 
  * @post 
  * 
  */
-void print(int array[], int total);
+void print(int array[], int length);
 	   
 int main() {
-  int array[MAX_ARRAY];
+  int *array;
+  int length = 0;
+  cout<<"Please insert an array size: ";
+  cin>>length;
+  cout<<"Thank you!"<<endl;
+
+  cout<<"Declaring Array..."<<endl;
+  array = new int[length]();
+  
   cout<<"Populating array...";
-  int total = populate(array);
+  populate(array, length);
   cout<<"Done!\n"<<endl;
   
   cout<<"Printing array contents..."<<endl;
-  print(array, total);
+  print(array, length);
   cout<<"Done!\n"<<endl;
 
   cout<<"Printing Average..."<<endl;
-  cout<<average(array, total)<<endl;
+  cout<<average(array, length)<<endl;
   cout<<"Done! Program Complete!"<<endl;
+
+  delete[] array;
+  array = NULL;
   
   return 0;
 }
 
-int populate(int array[]){
+void populate(int array[], int length){
   ifstream file;
   file.open("integar.txt");
 
   int i = 0;
   file>>array[i];
-  while (file){
+  while (file>>array[i]&&i<length){
     i++;
-    file>>array[i];
   }
   file.close();
-
-  return i;
 }
 
-float average(int array[], int total){
+float average(int array[], int length){
   float average = 0;
-  for (int i = 0; i < total; i++){
+  for (int i = 0; i < length; i++){
     average += array[i];
     //adds everything from the array into a singular integar.
   }
-  average /= total;
+  average /= length;
   //divides the average from the total
   
   return average; 
 }
 
-void print(int array[], int total){
-  for (int i = 0; i < total; i++){
+void print(int array[], int length){
+  for (int i = 0; i < length; i++){
     cout<<array[i]<<endl;
   }
 }
