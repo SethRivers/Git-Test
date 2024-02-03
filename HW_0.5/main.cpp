0e7021a3c69f0a94f6ec82859ae92a040a261851
Seth Rivers <davmrive@ut.utm.edu>
2024-02-02 21:42:25 -0600

    Completed #2 of HW0.5; hoping to redo questions 1, 2, 3, and 5

--- HW_0.5/main.cpp
+++ HW_0.5/main.cpp
@@ -2,7 +2,9 @@
  * @file main.cpp
  * @author Seth Rivers
  * @date 2024-02-01
- * @brief this branch is dedicated to redoing questions 1, 2, 3 and 5
+ * @brief this program is dedicated to redoing questions 1, 2, 3 and 5 (or just 2 and
+ * 3; with the first commit being the 1 and 5 version. I just tried to fit everything
+ * into the one file for ease of access)
  * 
  * 
  */
@@ -13,6 +15,10 @@
 #include <iomanip>
 #include <string>
 using namespace std;
+/*
+constint MAX_ARRAY = 1000;
+*originally used for number 1 of HW0.5 (redoing 1 and 5 on the quiz)
+*/
 
 /**
  * A function that populates the array with 100 integars from the txt file "integars"
@@ -49,14 +55,22 @@ float average(int array[], int length);
  * 
  */
 void print(int array[], int length);
-	   
+
+//base code used for the original question 1 (1/5 quiz redo)
 int main() {
+  //HW0.5(question 2 and 3 redo) version of code  
   int *array;
   int length = 0;
+
+  /*original line of code
+  int array[MAX_ARRAY];
+  */
+
+  //added for Number 2 of HW0.5 (quiz redo 2/3)
   cout<<"Please insert an array size: ";
   cin>>length;
   cout<<"Thank you!"<<endl;
-
+  
   cout<<"Declaring Array..."<<endl;
   array = new int[length]();
   
@@ -84,7 +98,17 @@ void populate(int array[], int length){
   for (int i = 0; i < length; i++){
     file >> array[i];
   }
-  
+
+  /* Original code for question 1(quiz redo1/5): 
+     
+     int i = 0;
+     file>>array[i];
+     while (file){
+       i++;
+       file>>array[i];
+  }
+  */  
+
   file.close();
 }
 
